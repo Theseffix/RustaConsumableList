c9f8395ca2a9ec971d53b654efe8d560da9ca101
Expell <57598916+Theseffix@users.noreply.github.com>
2021-04-20 11:17:06 +0200

    Initial

---
+++ Models/ConsumptionProduct.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace RustaConsumerList.Models
+{
+    public class ConsumptionProduct
+    {
+        //[Column ("Assortment Node")]
+        public double Id { get; set; }
+        //[Column ("Assortment Description")]
+        public string Name { get; set; }
+        //[Column("Category")]
+        public string Category { get; set; }
+    }
+}
---
+++ Models/EFContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace RustaConsumerList
+{
+
+    /*Install-Package Microsoft.Extensions.Configuration
+    Install-Package Microsoft.Extensions.Configuration.FileExtensions
+    Install-Package Microsoft.Extensions.Configuration.Json
+    Install-Package Microsoft.EntityFrameworkCore.SqlServer
+    Install-Package Microsoft.EntityFrameworkCore.Tools
+     
+     */
+
+    class EFContext : DbContext
+    {
+        private string connectionString;
+
+        public EFContext() : base()
+        {
+            var builder = new ConfigurationBuilder();
+            builder.AddJsonFile("appsettings.json", optional: false);
+
+            var configuration = builder.Build();
+            connectionString = configuration.GetConnectionString("sqlConnection");
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        //Bygg nya tables
+        public DbSet<Models.ConsumptionProduct> Products { get; set; }
+        //Skjut ut databas genom Add-Migration i Package Manager Console. Ex. Add-Migration "V2"
+    }
+}
